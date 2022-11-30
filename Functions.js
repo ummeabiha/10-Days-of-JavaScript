7c59f9072905683a1e9b428f3ee733e82e1ddbde
Syeda Umm E Abiha Rizvi <98107411+ummeabiha@users.noreply.github.com>
2022-12-01 02:40:08 +0500

    Functions

---
+++ Functions.js
@@ -0,0 +1,14 @@
+/*
+ * Create the function factorial here
+ */
+
+function factorial(n){
+    
+    let factorial = 1;
+    
+    for(let i=1; i<=n; i++){
+        factorial *=  i; 
+    }
+    
+    return factorial;
+}

cdd6a46fcdb24044a730f3f32660f3d18a80f65a
Syeda Umm E Abiha Rizvi <98107411+ummeabiha@users.noreply.github.com>
2022-12-01 02:41:21 +0500

    Let And Const

---
+++ LetAndConst.js
@@ -0,0 +1,11 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI= Math.PI;
+    let r= parseFloat(readLine());
+    
+    let area= PI*r*r;
+    console.log(area);
+    
+    let perimeter= 2*PI*r;
+    console.log(perimeter);
+    

730c8fd2e59bbeb26fb7d9cb30ac2355ed73ffff
Syeda Umm E Abiha Rizvi <98107411+ummeabiha@users.noreply.github.com>
2022-12-02 17:40:09 +0500

    Loop

---
+++ Loops.js
@@ -0,0 +1,24 @@
+function vowelsAndConsonants(s) {
+    for (var i=0; i<s.length; i++)
+    {
+        if (s[i]=='a' || s[i]=='e' ||s[i]=='i' ||s[i]=='o' ||s[i]=='u')
+        {
+          console.log(s[i]);  
+        }
+        
+    }
+    
+    for (var i=0; i<s.length; i++)
+    {
+        if (s[i]=='a' || s[i]=='e' ||s[i]=='i' ||s[i]=='o' ||s[i]=='u')
+        {
+           
+        }
+        
+        else
+        {
+            console.log(s[i]);
+        }
+    }
+ 
+}
